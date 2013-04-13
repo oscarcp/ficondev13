6da3bd064a50dafbcada0ded26f176a29a13b28f
Oscar Carballal Prego <info@oscarcp.com>
2013-04-13 23:18:31 +0200

    Moved all the code to a new location. Added READEME file and fixed index for passing HTML5 tests

--- frontend/js/site.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    menu = $('li a').not('.exclude');
-    menu.click(function(e){
-        $('.content').fadeOut('fast').load($(this).attr('href'));
-        $('.content').fadeIn();
-        return false;
-    });
-});
